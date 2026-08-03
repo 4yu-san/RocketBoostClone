6d3fe5c2f2b934b29a2c44b489b739e0319baa4a
4yu-san <abhyankaraayush@gmail.com>
2026-08-04 00:25:35 +0530

    Scenes Loop done

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -11,13 +12,31 @@ public class CollisionHandler : MonoBehaviour
                 break;
             case "Finish":
                 Debug.Log("Finish");
+                LoadNextLevel();
                 break;
             case "Fuel":
                 Debug.Log("Fuel");
                 break;
             default:
                 Debug.Log("Dead");
+                ReloadLevel();
                 break;
+        }
+
+        void LoadNextLevel(){
+            
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            int nextScene = currentScene + 1;
+            if(nextScene == SceneManager.sceneCountInBuildSettings)
+            {
+                nextScene = 0;
+            }
+            SceneManager.LoadScene(nextScene);
+        }
+        void ReloadLevel()
+        {
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentScene);
         }
     }
 }
